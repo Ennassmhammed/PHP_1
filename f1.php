913afb7ae6d95abb1153cff8753014c5177b61b0
ennassmhammed <ennassirimohammed14@gmail.com>
2025-10-16 17:12:40 +0200

    commit exercice f1

--- f1.php
+++ f1.php
@@ -1,11 +1,48 @@
-<h1>Exercice 1</h1>
-
-<p>Soit la phrase « Notre formation DL commence aujourd'hui ».
-Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase 
-(espaces inclus).</p>
-
+<h>Introduction PHP </p>
 <?php
-echo "<script src=\"https://shrek.hexanti.fr/dist/shrek-all.min.js\"></script>";
-$phrase = "Notre formation DL commence aujourd'hui";
-$nbCaracteres = strlen($phrase);
-echo "la phrase contient $nbCaracteres caractères";
+
+/*
+commentaire sur plusieur lignes*/
+
+
+// DECLARATION VARIABLE 
+$chaineDeCaracteres = "Mon texte"; //string
+$chaineDeCaracteres2 = "mon prix est trés interessant"; //string
+$chaineDeCaracteres3 = "Mon prix est de ";
+$entier = 50; //integer//
+$flottant = 9.99; //float//
+$booleen = true; //boolean (true ou false)//
+$tableau1 = array("texte 1", "texte 2", "texte 3");// array//
+$tableau2 = array("texte 1", "texte 2"); // array 
+$date = new DateTime(); // programmation orinaté objet (POO)
+
+//AFFICHAGE 
+echo "test<br>"; 
+echo $chaineDeCaracteres."<br>"; // mon texte 
+echo $chaineDeCaracteres3." ".$entier. " euro<br>";  // Mon prix est de 50 euros 
+echo "$chaineDeCaracteres3 $entier euro<br>"; 
+
+//FONCTION CHAINE DE CARACTERES
+
+// COMPTER LE NOMBRE DE CARACTERE (Espace inclus !)
+$nbCaracteres = strlen($chaineDeCaracteres); 
+echo "la phrase contient $nbCaracteres caracteres<br>"; 
+echo "La phrase contient ".strlen($chaineDeCaracteres)."caracteres<br>"; 
+
+
+// Convertir une chaine en majuscule 
+$chaineMajuscules = strtoupper($chaineDeCaracteres); 
+echo $chaineMajuscules."<br>";
+//echo mb_strtouppeur($chaineDeCaracteres2);    //mb_strtouppe pour convertire les caracteres accentuer mais puisque jen est pas mis jen est pas besoin //
+
+
+// Compter le nombre de mots 
+$nbMots = str_word_count($chaineDeCaracteres); 
+echo "La phrase contient $nbMots mots<br>";
+// FONCTIONS TABLEAUX
+$tailleTablea1 = count($tableau1); 
+echo "le tableau 1 contient $taileTanleau1 éléments<br>"; 
+echo "Le tableau 2 contient  ".count($tableau2)."éléments<br>"; 
+
+echo $tableau1[3]; // accéder a la première valeur de tableau 
+
